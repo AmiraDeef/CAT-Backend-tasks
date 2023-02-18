58fa519a0eb5df1c497eb33f2f443814f56ae3f7
AmiraDeef <amiradeef4@gmail.com>
2023-02-18 19:16:20 +0200

    last prob added

---
+++ problems/prob15.sql
@@ -0,0 +1,1 @@
+select s.Name from Students s join Friends f on s.ID=f.ID join Packages student_salary on s.ID=student_salary.ID join Packages friend_salary on f.Friend_Id=friend_salary.ID where friend_salary.Salary>student_salary.Salary order by friend_salary.Salary;
